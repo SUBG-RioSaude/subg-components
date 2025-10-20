e98334d741e07595a51daae1b4c267ff15476eb1
Matheus Migliani <matheusmigliani@yahoo.com.br>
2025-10-20 18:01:41 -0300

    fix: emit tailwind plugin types

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
 
 const srcDir = resolve(__dirname, 'src')
 
 export default defineConfig({
   plugins: [
     react(),
-    dts({
-      entryRoot: 'src',
-      include: ['src'],
-      tsconfigPath: 'tsconfig.json',
-      rollupTypes: true,
-    }),
   ],
   build: {
     lib: {
